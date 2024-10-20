168ec3911000674881d9dd70bd5be3d9e33813e5
hackartists <hackartists@gmail.com>
2024-10-20 16:29:51 +0900

    update version

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ mod lambda;
 
 #[doc = include_str!("../docs/launch.md")]
 pub fn launch(app: fn() -> Element) {
-    #[cfg(not(feature = "lambda"))]
+    #[cfg(feature = "web")]
+    dioxus::launch(app);
+
+    #[cfg(all(not(feature = "lambda"), feature = "server"))]
     dioxus::launch(app);
 
     #[cfg(feature = "lambda")]
@@ -13,12 +16,24 @@ pub fn launch(app: fn() -> Element) {
         use axum::routing::*;
         use dioxus_fullstack::prelude::*;
 
+        struct TryIntoResult(Result<ServeConfig, dioxus_fullstack::UnableToLoadIndex>);
+
+        impl TryInto<ServeConfig> for TryIntoResult {
+            type Error = dioxus_fullstack::UnableToLoadIndex;
+
+            fn try_into(self) -> Result<ServeConfig, Self::Error> {
+                self.0
+            }
+        }
+
         tokio::runtime::Runtime::new()
             .unwrap()
             .block_on(async move {
                 use self::lambda::LambdaAdapter;
-                let app = Router::new()
-                    .serve_dioxus_application(ServeConfigBuilder::default().build(), app);
+                let app = Router::new().serve_dioxus_application(
+                    TryIntoResult(ServeConfigBuilder::default().build()),
+                    app,
+                );
 
                 tracing::info!("Running in lambda mode");
                 lambda_runtime::run(LambdaAdapter::from(app)).await.unwrap();
